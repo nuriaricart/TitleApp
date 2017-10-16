ba382fd66a0918d43205928d2df0931ca8b793d8
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-16 16:12:17 +0200

    5) Ja podem editar el títol

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -9,23 +9,41 @@ import android.widget.TextView;
 public class TitleActivity extends AppCompatActivity {
 
     private String title = "This is the initial title";
+    private TextView title_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        TextView title_text = (TextView) findViewById(R.id.title);
+        title_text = (TextView) findViewById(R.id.title);
         title_text.setText(title);
 
     }
 
     public void editTitle(View view) {
+        // (I)
         //1. Crear un 'intent' --> Intent (classe a la que estic, classe a la que vull cridar)
         Intent intent =  new Intent(this, EditTitleActivity.class);
         //2. Afegir paràmetres (dades extra) a la crida a l'activitat
-        intent.putExtra("title", title)
+        intent.putExtra("title", title);
         //3. Passar l'intent a Android perque obri l'activitat
-        startActivity(intent);
+        //startActivity(intent);
+        //Que t'arribi el resultat d'una altra activitat
+        startActivityForResult(intent, 0); // <-- espero un resultat d'aixo
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 0:
+                // (IV)
+                if (resultCode == AppCompatActivity.RESULT_OK){
+                    String new_title = data.getStringExtra("title");
+                    title = new_title;
+                    title_text.setText(title);
+                }
+        }
+    }
+
 }
